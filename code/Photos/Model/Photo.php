5ef61e22a371d983e9264d701b435ffac8eb3f88
Cristian Quiroz <cdquirozc@gmail.com>
2015-04-20 20:34:56 +0100

    Photo model to create thumbnail while loading photo data

--- code/Photos/Model/Photo.php
+++ code/Photos/Model/Photo.php
@@ -1,6 +1,8 @@
 <?php
 namespace Photos\Model;
 
+use \Eventviva\ImageResize;
+
 class Photo
 {
 
@@ -77,6 +79,41 @@ class Photo
             return array();
         }
 
-        return exif_read_data($pathToPhoto);
+        $photoData = exif_read_data($pathToPhoto);
+        $photoData['Thumbnail'] = $this->getPathToThumbnail($pathToPhoto);
+
+        return $photoData;
+    }
+
+    /**
+     * @param string $pathToPhoto
+     * @return string
+     * @author Cristian Quiroz <cris@qrz.io>
+     */
+    public function getPathToThumbnail($pathToPhoto)
+    {
+        $thumbDirectory = pathinfo($pathToPhoto, PATHINFO_DIRNAME) . '/.thumb/';
+        $pathToThumb =  $thumbDirectory . pathinfo($pathToPhoto, PATHINFO_BASENAME);
+
+        if (!file_exists($pathToThumb)) {
+            $this->resizeImage($pathToPhoto, $pathToThumb);
+        }
+
+        return $pathToThumb;
+    }
+
+    /**
+     * @param $pathToImage
+     * @param $pathToSave
+     * @return Photo
+     * @author Cristian Quiroz <cris@qrz.io>
+     */
+    public function resizeImage($pathToImage, $pathToSave)
+    {
+        $image = new ImageResize($pathToImage);
+        $image->resizeToWidth(300);
+        $image->save($pathToSave);
+
+        return $this;
     }
 }
